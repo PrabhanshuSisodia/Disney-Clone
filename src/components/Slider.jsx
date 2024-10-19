69a011d9ca57db4fd1877064d3b8b016c53a00d3
PrabhanshuSisodia <psisodia2001@gmail.com>
2024-10-19 23:18:37 +0530

    Commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useState} from "react"
 import GlobalApi from "../services/GlobalApi"
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
 function Slider() {
+    const [movieList, setMovieList] = useState([]);
+
     useEffect(() => {
         getTrendingMovies();
     },[])
 
     const getTrendingMovies = () => {
         GlobalApi.getTrendingVideos.then(resp => {
-            console.log(resp);
+            console.log(resp.data.results);
+            setMovieList(resp.data.results);
         })
     }
   return (
     <div>
-      
+          {movieList.map((item) => (
+          <img key={item.name} src={IMAGE_BASE_URL+item.profile_path} alt="" className="min-w-full h-[310px]"/>
+      ))}
     </div>
   )
 }
--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const movieBaseUrl = "https://api.themoviedb.org/3";
-const api_key = "9e47026687cbf24e34cab4aa28e2ee74";
+const api_key = "9b714491fd841e3c2d38fe8847b133a8";
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=9e47026687cbf24e34cab4aa28e2ee74
 
-const getTrendingVideos = axios.get(`${movieBaseUrl} /trending/all/day?api_key= ${api_key}`);
+const getTrendingVideos = axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
 
 export default {
     getTrendingVideos
